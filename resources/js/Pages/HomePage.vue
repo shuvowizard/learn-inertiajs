02bab796a20d797a32a639d69f2769894201aa6e
Shuvo <shuvot01794@gmail.com>
2026-05-19 20:39:58 +0600

    feat: setup Laravel with Inertia.js and Vue, and render initial page

---
+++ resources/js/Pages/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Home Page" />
+    <div class="container">
+        <h1 class="text-3xl font-bold underline">Home Page</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import inertia from '@inertiajs/vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+            fonts: [
+                bunny('Instrument Sans', {
+                    weights: [400, 500, 600],
+                }),
+            ],
+        }),
+        tailwindcss(),
+        vue(),
+        inertia(),
+    ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
